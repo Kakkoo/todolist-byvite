5e7179b700da60a39e70067bde04b5982d271f8f
Kanchan Sharma <58194538+Kakkoo@users.noreply.github.com>
2025-02-20 10:28:56 -0800

    Update vitte.config.js  by removing testing configuration

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-  },
+
   "compilerOptions": {
     "types": ["vitest/globals", "@testing-library/jest-dom"]
   },
