843d2dc93c9c26f42b66c1abb225996cff697a5e
kakkoo <kanchanji@gmail.com>
2025-02-21 09:44:52 -0800

    d

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
+  // plugins: [react()],
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  //   setupFiles: ['./src/setupTests.ts'],
+  // },
   "compilerOptions": {
     "types": ["vitest/globals", "@testing-library/jest-dom"]
   },
